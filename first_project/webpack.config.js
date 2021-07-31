db2f0f7be2ad66fad751e16c757a45c6c177a775
Javier Chavez <javier120699lili@gmail.com>
2021-07-31 00:23:06 -0500

    Using enviroment vars

--- first_project/webpack.config.js
+++ first_project/webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const CopyWebpackPlugin = require("copy-webpack-plugin")
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
 const TerserPlugin = require("terser-webpack-plugin")
+const Dotenv = require("dotenv-webpack")
 
 module.exports = {
     entry: "./src/index.js",
@@ -76,6 +77,7 @@ module.exports = {
                 },
             ],
         }),
+        new Dotenv(),
     ],
     optimization: {
         minimize: true,
